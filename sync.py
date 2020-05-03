a22c19b7bd03d0ee480c0f90563abf06548e3061
Sandy Fernando Perez Matamoros <sandypm007@gmail.com>
2020-05-03 03:03:22 -0400

    Adding try catch

--- sync.py
+++ sync.py
@@ -79,7 +79,6 @@ def init(token):
 
 if __name__ == "__main__":
     try:
-        a = 1 / 0
         local_repo = os.getenv("REPO")
         if not os.path.exists(local_repo):
             print("Folder should exists")
